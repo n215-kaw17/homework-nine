6d78e2929a0df28fd0e7202689a4e96d7eb9277c
kaw17 <kaw17@iu.edu>
2023-04-17 08:54:59 -0400

    Added file

---
+++ model/model.js
@@ -0,0 +1,22 @@
+export var wordArray = ["home", "about", "products", "contact"];
+
+export function changeRoutes(){
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+
+    if (pageID != ""){
+        $.get(`pages/${pageID}/${pageID}.html`, function(data){
+            console.log("data " + data);
+            $("#app").html(data);
+        })
+        console.log(pageID)
+    }
+    else{
+        $.get(`pages/home/home.html`, function (data) {
+            console.log("data " + data);
+            $("#app").html(data);
+        });
+        console.log("home");
+    }
+}
+
